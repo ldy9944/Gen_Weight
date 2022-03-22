e49b6e62774ff14286fde253b3fb7e08f67c808e
DONGYEON LEE <83826792+ldy9944@users.noreply.github.com>
2022-03-22 19:46:03 +0900

    Update gen_weight_calculator.py

--- gen_weight_calculator.py
+++ gen_weight_calculator.py
@@ -1,18 +1,29 @@
-# 身長及び性別をinput
-user_gender = input('性別を入力してください')
-user_height = int(input('身長を数字のみで入力してください(cm)'))
+while True:
+    # 身長及び性別をinput, 入力が間違えると再起動
+    user_gender = input('性別を入力してください')
 
-# 様々な性別入力バリエーションに合わせて入力値を自動修正
-m_list = ['男性', 'Man', 'man', 'Men', 'men' 'Male', 'male', '男子', '男']
-f_list = ['女性', 'Woman', 'woman' 'Women', 'women', 'Female', 'female', '女子', '女']
+    try:
+        user_height = int(input('身長を数字のみで入力してください(cm)'))
 
-if user_gender in m_list:
-    user_gender = 'M'
-elif user_gender in f_list:
-    user_gender = 'F'
-else:
-    print('性別の入力を確かめてください。')
-    print()
+    except ValueError:
+        print('身長は数字のみ入力してください。\nプログラムを再起動します。')
+        print()
+        continue
+
+    # 様々な性別入力バリエーションに合わせて入力値を自動修正、入力が間違えると再起動
+    m_list = ['男性', 'Man', 'man', 'Men', 'men' 'Male', 'male', '男子', '男', 'M', 'm']
+    f_list = ['女性', 'Woman', 'woman' 'Women', 'women', 'Female', 'female', '女子', '女', 'F', 'f']
+
+    if user_gender in m_list:
+        user_gender = 'M'
+        break
+    elif user_gender in f_list:
+        user_gender = 'F'
+        break
+    else:
+        print('性別の入力を確かめてください。\nプログラムを再起動します。')
+        print()
+        continue
 
 # 標準体重計算
 if user_gender == 'M':
